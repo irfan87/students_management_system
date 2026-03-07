3a9ece412f7be4446b0c8a9f1d1452382cf3039b
irfan87 <nerve2009@yahoo.com>
2026-03-08 04:44:31 +0800

    please work

--- db/migrate/20260307200048_change_teacher_id_to_integer_in_course_sessions.rb
+++ db/migrate/20260307200048_change_teacher_id_to_integer_in_course_sessions.rb
@@ -1,22 +1,51 @@
 class ChangeTeacherIdToIntegerInCourseSessions < ActiveRecord::Migration[8.1]
   def up
-    # Make sure no invalid data exists first
-    execute <<-SQL
-      UPDATE course_sessions
-      SET teacher_id = NULL
-      WHERE teacher_id !~ '^\d+$';
-    SQL
+    # 1️⃣ Add temporary integer column
+    add_column :course_sessions, :teacher_id_temp, :integer
 
-    # Change column type using proper cast
-    change_column :course_sessions, :teacher_id, :integer, using: 'teacher_id::integer'
+    adapter = ActiveRecord::Base.connection.adapter_name.downcase
 
-    # Add foreign key
+    if adapter.include?("sqlite")
+      execute <<-SQL
+        UPDATE course_sessions
+        SET teacher_id_temp = CAST(teacher_id AS INTEGER);
+      SQL
+
+    elsif adapter.include?("postgresql")
+      # Remove invalid values before casting
+      execute <<-SQL
+        UPDATE course_sessions
+        SET teacher_id = NULL
+        WHERE teacher_id !~ '^[0-9]+$';
+      SQL
+
+      execute <<-SQL
+        UPDATE course_sessions
+        SET teacher_id_temp = teacher_id::integer;
+      SQL
+    end
+
+    # 3️⃣ Remove old column
+    remove_column :course_sessions, :teacher_id
+
+    # 4️⃣ Rename temp column
+    rename_column :course_sessions, :teacher_id_temp, :teacher_id
+
+    # 5️⃣ Add foreign key
     add_foreign_key :course_sessions, :teachers
   end
 
   def down
     remove_foreign_key :course_sessions, :teachers
 
-    change_column :course_sessions, :teacher_id, :string, using: 'teacher_id::text'
+    add_column :course_sessions, :teacher_id_temp, :string
+
+    execute <<-SQL
+      UPDATE course_sessions
+      SET teacher_id_temp = teacher_id;
+    SQL
+
+    remove_column :course_sessions, :teacher_id
+    rename_column :course_sessions, :teacher_id_temp, :teacher_id
   end
 end
