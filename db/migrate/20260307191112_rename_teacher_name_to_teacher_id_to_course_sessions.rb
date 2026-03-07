99b4d9139f65b777f319e65e78011577c5a2c1f6
irfan87 <nerve2009@yahoo.com>
2026-03-08 03:56:15 +0800

    remove certain migration files

--- db/migrate/20260307191112_rename_teacher_name_to_teacher_id_to_course_sessions.rb
+++
@@ -1,5 +0,0 @@
-class RenameTeacherNameToTeacherIdToCourseSessions < ActiveRecord::Migration[8.1]
-  def change
-    rename_column :course_sessions, :teacher_name, :teacher_id
-  end
-end
--- db/migrate/20260307191455_fix_course_session_foriegn_key.rb
+++
@@ -1,5 +0,0 @@
-class FixCourseSessionForiegnKey < ActiveRecord::Migration[8.1]
-  def change
-    add_foreign_key :course_sessions, :teachers, column: :teacher_id
-  end
-end
