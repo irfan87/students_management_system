2a2c5dbc2bf28e012e3e62bce4e57b8ca91a0dc8
irfan87 <nerve2009@yahoo.com>
2026-03-08 04:38:13 +0800

    please....

--- db/migrate/20260307200048_change_teacher_id_to_integer_in_course_sessions.rb
+++ db/migrate/20260307200048_change_teacher_id_to_integer_in_course_sessions.rb
@@ -1,42 +1,22 @@
 class ChangeTeacherIdToIntegerInCourseSessions < ActiveRecord::Migration[8.1]
   def up
-    if ActiveRecord::Base.connection.adapter_name.downcase.include?("sqlite")
-      # SQLite flow: temp column
-      add_column :course_sessions, :teacher_id_temp, :integer
+    # Make sure no invalid data exists first
+    execute <<-SQL
+      UPDATE course_sessions
+      SET teacher_id = NULL
+      WHERE teacher_id !~ '^\d+$';
+    SQL
 
-      execute <<-SQL
-        UPDATE course_sessions
-        SET teacher_id_temp = CAST(teacher_id AS INTEGER);
-      SQL
+    # Change column type using proper cast
+    change_column :course_sessions, :teacher_id, :integer, using: 'teacher_id::integer'
 
-      remove_column :course_sessions, :teacher_id
-      rename_column :course_sessions, :teacher_id_temp, :teacher_id
-    else
-      # PostgreSQL flow: change_column using cast
-      change_column :course_sessions, :teacher_id, :integer, using: 'teacher_id::integer'
-    end
-
-    # Add foreign key (works for both SQLite and Postgres)
+    # Add foreign key
     add_foreign_key :course_sessions, :teachers
   end
 
   def down
     remove_foreign_key :course_sessions, :teachers
 
-    if ActiveRecord::Base.connection.adapter_name.downcase.include?("sqlite")
-      # Reverse SQLite flow
-      add_column :course_sessions, :teacher_id_temp, :string
-
-      execute <<-SQL
-        UPDATE course_sessions
-        SET teacher_id_temp = teacher_id;
-      SQL
-
-      remove_column :course_sessions, :teacher_id
-      rename_column :course_sessions, :teacher_id_temp, :teacher_id
-    else
-      # Reverse PostgreSQL flow
-      change_column :course_sessions, :teacher_id, :string, using: 'teacher_id::text'
-    end
+    change_column :course_sessions, :teacher_id, :string, using: 'teacher_id::text'
   end
 end
